5177caaa927a540d233bbc006a06548daec16ae8
Andres Kirschbaum <andres@parsimotion.com>
2013-09-23 14:11:02 -0300

    Ahora se pueden eliminar filas

--- customBindings/handsontableBinding.js
+++ customBindings/handsontableBinding.js
@@ -4,15 +4,17 @@ ko.bindingHandlers.handsontable = {
      var colHeaders = ["Plain text title", "HTML Description", "Cover", "Country"];
 
      $(element).handsontable({
-         data: value(),
-         minSpareRows: 1,
-         dataSchema: function(){ return new Album({ title: null, description: null, cover: null, country: null }) },
-            columns: [
-                {data: property("title"), width: "200"},
-                {data: property("description"), width: "200" },
-                {data: property("cover"), width: "200" },
-                {data: property("country"), type: "select2", width: "200", selectorData: [{id:'AR',text:'Argentina'},{id:'BR',text:'Brasil'},{id:'CH',text:'Chile'},{id:'UY',text:'Uruguay'}] }
-            ]
+        data: value(),
+        minSpareRows: 1,
+        dataSchema: function(){ return new Album({ title: null, description: null, cover: null, country: null }) },
+        columns: [
+            {data: property("title"), width: "200"},
+            {data: property("description"), width: "200" },
+            {data: property("cover"), width: "200" },
+            {data: property("country"), type: "select2", width: "200", selectorData: [{id:'AR',text:'Argentina'},{id:'BR',text:'Brasil'},{id:'CH',text:'Chile'},{id:'UY',text:'Uruguay'}] }
+        ],
+        removeRowPlugin: true,
+        removeRow: function(row){ value.remove(value()[row]); }
      });
 
  },
