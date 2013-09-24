edd956dfa78a7ed7f80a79bcfc65d9622e7e8922
Andres Kirschbaum <andres@parsimotion.com>
2013-09-24 15:20:27 -0300

    Un boludez

--- onLoad.js
+++ onLoad.js
@@ -12,9 +12,13 @@ function ViewModel(albums){
   }
 
   this.setAllCountriesAR = function(){
-    self.albums().forEach(function(item){
+    self.realAlbums().forEach(function(item){
       item.country(['AR']);
     });
+  }
+
+  this.realAlbums = function(){
+    return self.albums().filter(function(album){ return self.albums().indexOf(album) < self.albums().length -1 });
   }
 
   this.removeAlbum = function(index){
@@ -33,19 +37,19 @@ $(document).ready(function () {
       title: "Professional JavaScript for Web Developers",
       description: "This <a href='http://bit.ly/sM1bDf'>book</a> provides a developer-level introduction along with more advanced and useful features of <b>JavaScript</b>.",
       cover: "http://ecx.images-amazon.com/images/I/51bRhyVTVGL._SL50_.jpg",
-      country: ["BR"]
+      country: "BR"
     },
     {
       title: "JavaScript: The Good Parts",
       description: "This book provides a developer-level introduction along with <b>more advanced</b> and useful features of JavaScript.",
       cover: "http://ecx.images-amazon.com/images/I/51gdVAEfPUL._SL50_.jpg",
-      country: ["UY"]
+      country: "UY"
     },
     {
       title: "JavaScript: The Definitive Guide",
       description: "<em>JavaScript: The Definitive Guide</em> provides a thorough description of the core <b>JavaScript</b> language and both the legacy and standard DOMs implemented in web browsers.",
       cover: "http://ecx.images-amazon.com/images/I/51VFNL4T7kL._SL50_.jpg",
-      country: ["AR"]
+      country: "AR"
     }
   ];
 
