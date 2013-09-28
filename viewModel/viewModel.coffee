8df8d320e89a9abf2a834b590ab81aebc8cb8953
Andres Kirschbaum <andres@parsimotion.com>
2013-09-28 15:53:01 -0300

    ViewModel en coffee

---
+++ viewModel/viewModel.coffee
@@ -0,0 +1,57 @@
+class Field
+  constructor:(data,val)->
+    ko.mapping.fromJS data, {}, this
+    @value(val) if typeof val != 'undefined'
+    delete @__ko_mapping__
+  adapt: (runner)->
+    runner.adaptField @
+
+class Selector extends Field
+  adapt: (runner)->
+    runner.adaptSelector @
+
+  getSelectorPair: (prop,value)->
+    @selectorData().filter((item)->item[prop]()==value)[0]
+  getDisplayValue: ()->
+    return "" if !@value()
+    @getSelectorPair('id',@value()).text()
+  setValue: (text)->
+    if !text
+      @value null
+    else
+      @value @getSelectorPair('text',text).id()
+
+class Row
+  constructor:(fieldsData,rowData)->
+    @fields = ko.observableArray fieldsData.map (field)->
+      new window[field._type](field,rowData[field.name])
+
+  getFieldByName: (name)->
+    @fields().filter((field)->field.name()==name)[0]
+
+class InputGrid
+  constructor: (fieldsData,data)->
+    @fieldsData = fieldsData
+    @fields = ko.observableArray fieldsData.map (field)->
+      new window[field._type](field)
+    @rows = ko.observableArray data.map (row)->
+      new Row(fieldsData,row)
+
+  setDateAsFirstTitle: ()->
+    @rows()[0].getFieldByName('title').value new Date()
+
+  setAllCountriesAR: ()->
+    @realRows().forEach (row)->row.getFieldByName('country').value 'AR'
+
+  realRows: ()->
+    @rows().filter (row)=> @rows().indexOf(row)<@rows().length-1
+
+  removeRow: (index)->
+    @rows.remove @rows()[index]
+
+  newRowTemplate: ()=>
+    new Row(@fieldsData,{})
+
+  dataMatrix: ()->
+    @rows().map (row)->
+      row.fields().map (field)-> field.value()
--- viewModel/viewModel.js
+++ viewModel/viewModel.js
@@ -1,85 +1,124 @@
-function Field(data){
-  var self = this;
-  ko.mapping.fromJS(data, {}, this);
-  this.adapt = function(runner){
-    return runner.adaptField(self);
-  }
-  delete this.__ko_mapping__;
-}
+var Field, InputGrid, Row, Selector, _ref,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-function Selector(data){
-  var self = this;
-  ko.mapping.fromJS(data, {}, this);
-  this.adapt = function(runner){
-    return runner.adaptSelector(self);
-  }
-  this.getSelectorPair = function(prop,value){
-    return self.selectorData().filter(function(item){ return item[prop]()==value })[0];
-  }
-  this.getDisplayValue = function(){
-    if(!self.value())
-      return "";
-    return self.getSelectorPair('id',self.value()).text();
-  }
-  this.setValue = function(text){
-    if(!text)
-      self.value(null);
-    else
-      self.value(self.getSelectorPair('text',text).id());
-  }
-  delete this.__ko_mapping__;
-}
-
-function Row(fields,data){
-  var self = this;
-  this.fields = ko.observableArray();
-
-  this.getFieldByName = function(name){
-    return self.fields().filter(function(field){return field.name()==name;})[0];
+Field = (function() {
+  function Field(data, val) {
+    ko.mapping.fromJS(data, {}, this);
+    if (typeof val !== 'undefined') {
+      this.value(val);
+    }
+    delete this.__ko_mapping__;
   }
 
-  fields.forEach(function(field){
-    newField = new window[field._type](field);
-    if (data)
-      newField.value(data[field.name]);
-    self.fields.push(newField);
-  });
-}
-
-function InputGrid(fields,data){
-  var self = this;
-  this.fields = ko.observableArray();
-  this.rows = ko.observableArray();
-
-  fields.forEach(function(field){
-    self.fields.push(new window[field._type](field))
-  });
-
-  data.forEach(function(row){
-    var newRow = new Row(fields,row);
-    self.rows.push(newRow);
-  });
-
-  this.setDateAsFirstTitle = function(){
-    self.rows()[0].getFieldByName('title').value(new Date());
+  Field.prototype.adapt = function(runner) {
+    return runner.adaptField(this);
   };
 
-  this.setAllCountriesAR = function(){
-    self.realRows().forEach(function(row){
-      row.getFieldByName('country').value('AR');
+  return Field;
+
+})();
+
+Selector = (function(_super) {
+  __extends(Selector, _super);
+
+  function Selector() {
+    _ref = Selector.__super__.constructor.apply(this, arguments);
+    return _ref;
+  }
+
+  Selector.prototype.adapt = function(runner) {
+    return runner.adaptSelector(this);
+  };
+
+  Selector.prototype.getSelectorPair = function(prop, value) {
+    return this.selectorData().filter(function(item) {
+      return item[prop]() === value;
+    })[0];
+  };
+
+  Selector.prototype.getDisplayValue = function() {
+    if (!this.value()) {
+      return "";
+    }
+    return this.getSelectorPair('id', this.value()).text();
+  };
+
+  Selector.prototype.setValue = function(text) {
+    if (!text) {
+      return this.value(null);
+    } else {
+      return this.value(this.getSelectorPair('text', text).id());
+    }
+  };
+
+  return Selector;
+
+})(Field);
+
+Row = (function() {
+  function Row(fieldsData, rowData) {
+    this.fields = ko.observableArray(fieldsData.map(function(field) {
+      return new window[field._type](field, rowData[field.name]);
+    }));
+  }
+
+  Row.prototype.getFieldByName = function(name) {
+    return this.fields().filter(function(field) {
+      return field.name() === name;
+    })[0];
+  };
+
+  return Row;
+
+})();
+
+InputGrid = (function() {
+  function InputGrid(fieldsData, data) {
+    this.newRowTemplate = __bind(this.newRowTemplate, this);
+    this.fieldsData = fieldsData;
+    this.fields = ko.observableArray(fieldsData.map(function(field) {
+      return new window[field._type](field);
+    }));
+    this.rows = ko.observableArray(data.map(function(row) {
+      return new Row(fieldsData, row);
+    }));
+  }
+
+  InputGrid.prototype.setDateAsFirstTitle = function() {
+    return this.rows()[0].getFieldByName('title').value(new Date());
+  };
+
+  InputGrid.prototype.setAllCountriesAR = function() {
+    return this.realRows().forEach(function(row) {
+      return row.getFieldByName('country').value('AR');
     });
   };
 
-  this.realRows = function(){
-    return self.rows().filter(function(row){ return self.rows().indexOf(row) < self.rows().length -1 });
+  InputGrid.prototype.realRows = function() {
+    var _this = this;
+    return this.rows().filter(function(row) {
+      return _this.rows().indexOf(row) < _this.rows().length - 1;
+    });
   };
 
-  this.removeRow = function(index){
-    self.rows.remove(self.rows()[index])
+  InputGrid.prototype.removeRow = function(index) {
+    return this.rows.remove(this.rows()[index]);
   };
 
-  this.newRowTemplate = function(){
-    return new Row(fields);
-  }
+  InputGrid.prototype.newRowTemplate = function() {
+    return new Row(this.fieldsData, {});
+  };
 
-}
+  InputGrid.prototype.dataMatrix = function() {
+    return this.rows().map(function(row) {
+      return row.fields().map(function(field) {
+        return field.value();
+      });
+    });
+  };
+
+  return InputGrid;
+
+})();
