78a5748302f5f312418ca679dd5ba84b2be9c7c2
Andres Kirschbaum <andres@parsimotion.com>
2013-09-30 16:56:41 -0300

    El mutliValue no tiene que ser la ultima columna
    Sino cuando lo seleccionas no scrollea

--- onLoad.js
+++ onLoad.js
@@ -5,7 +5,8 @@ $(document).ready(function () {
     { name: 'cover', type: 'text', text: "Cover", value: null, _type: "Field" },
     { name: 'date', type: 'date', text: "Date", value: null, _type: "Field" },
     { name: 'country', type: 'autocomplete', text: "Country", value: null, _type: "Selector", selectorData: [{id:'AR',text:'Argentina'},{id:'BR',text:'Brasil'},{id:'CH',text:'Chile'},{id:'UY',text:'Uruguay'}] },
-    { name: 'authors', type: 'multiValue', text: "Authors", value: null, _type: "MultiValue", selectorData: [{id:'CP',text:'Charles Perez'},{id:'NG',text:'Nick Gomez'},{id:'WW',text:'Walter White'},{id:'ER',text:'Edward Rodriguez'}] }
+    { name: 'authors', type: 'multiValue', text: "Authors", value: null, _type: "MultiValue", selectorData: [{id:'CP',text:'Charles Perez'},{id:'NG',text:'Nick Gomez'},{id:'WW',text:'Walter White'},{id:'ER',text:'Edward Rodriguez'}] },
+    { name: 'notes', type: 'text', text: "Notes", value: null, _type: "Field" },
   ];
 
   var data = [
